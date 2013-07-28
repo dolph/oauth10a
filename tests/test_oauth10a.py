1583301c9f113fbfa1869ad0596a55cdbda770f9
Dolph Mathews <dolph.mathews@gmail.com>
2013-07-27 22:59:39 -0500

    added trivial test

---
+++ tests/test_oauth10a.py
@@ -0,0 +1,14 @@
+import unittest
+
+import oauth10a
+
+
+class TestOAuth10a(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_trivial(self):
+        self.assertTrue(oauth10a)
