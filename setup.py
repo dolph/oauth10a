d4c8fc7ba1dc7af069eda963b7b9e4868d3f8546
Dolph Mathews <dolph.mathews@gmail.com>
2013-07-27 22:21:45 -0500

    added empty package & setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+
+setuptools.setup(
+    name='oauth10a',
+    version='0.1.0',
+    description='OAuth 1.0a for consumers and service providers.',
+    author='Dolph Mathews',
+    author_email='dolph.mathews@gmail.com',
+    url='http://github.com/dolph/oauth10a',
+    packages=['oauth10a'],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Security',
+        'Topic :: Software Development :: Libraries',
+    ],
+)
