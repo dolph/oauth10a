b9a27b0b65e20cc01f4da168a5334568252fa32b
Dolph Mathews <dolph.mathews@gmail.com>
2013-08-01 22:10:55 -0500

    switch to GET for term.ie

--- tests/test_consumer.py
+++ tests/test_consumer.py
@@ -7,12 +7,15 @@ class TestCase(test_oauth10a.TestCase):
     def test_trivial(self):
         self.assertTrue(consumer)
 
-    def test_term_ie(self):
+    def test_termie(self):
         request_token_url = 'http://term.ie/oauth/example/request_token.php'
         consumer_key = 'key'
         consumer_secret = 'secret'
 
         r = consumer.obtain_request_token(
-            request_token_url, consumer_key, consumer_secret)
+            request_token_url,
+            consumer_key,
+            consumer_secret,
+            http_method='GET')
         self.assertEqual(
             r.text, 'oauth_token=requestkey&oauth_token_secret=requestsecret')
